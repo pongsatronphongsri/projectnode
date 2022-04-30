50ea5d7e3129a4b4b72080ce4e23afc084a90298
pongsatronphongsri <6306021622011@fitm.kmutnb.ac.th>
2022-04-30 12:04:19 +0700

    update

--- middlewares/currentUrl.js
+++
@@ -1,6 +0,0 @@
-const currentUrl=(req,res,next)=>{
-    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-    req.session.previousUrl=fullUrl;
-    next();   
-}
-module.exports=currentUrl;
